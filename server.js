e9b00004d30b19f1f7e84bf338a4aef6b12ca5d8
GilGuereque <gguereque326@gmail.com>
2023-12-25 17:51:45 -0800

    updating server.js

--- server.js
+++ server.js
@@ -44,8 +44,3 @@ app.use('/todos', todoRoutes)
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
 })  
-
-
-// test commit
-// hopefully this commit logs into my profile successfully
-const start = "test variable"
